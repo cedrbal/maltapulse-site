80b8a288362133dab5801279ce2c86300b42ef8c
cedrbal <cedricbaldacchino@gmail.com>
2026-03-30 16:14:56 +0200

    Auto-update API URL to https://everybody-quotations-laboratory-laundry.trycloudflare.com

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://ghz-given-idaho-arcade.trycloudflare.com";
+export const API_URL = "https://everybody-quotations-laboratory-laundry.trycloudflare.com";
 
 export async function fetchPosts() {
   try {
